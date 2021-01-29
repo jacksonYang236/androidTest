5f6e9fefb401eff4db0cdef447b1a320f4d37dbc
YSQ <844537803@qq.com>
2021-01-29 16:33:22 +0800

    Third -As

--- app/src/main/java/com/imooc/broadcasttest/MainActivity.java
+++ app/src/main/java/com/imooc/broadcasttest/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //大家好
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.com.CONNECTIVITY_CHANGE");
         networkReceiver = new NetworkReceiver();
